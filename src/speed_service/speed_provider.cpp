d0b451c19b63fa61ba1b4ee8ec5d3859334fda54
stawank <stawank@gmail.com>
2026-03-24 15:30:17 +0100

    feat: SpeedProvider fires ET_FIELD events at 200ms interval

--- src/speed_service/speed_provider.cpp
+++ src/speed_service/speed_provider.cpp
@@ -3,6 +3,7 @@
 #include <vsomeip/runtime.hpp>
 #include <thread>
 #include <atomic>
+#include <cstring>
 
 class SpeedProvider{
     
@@ -10,6 +11,15 @@ class SpeedProvider{
     std::shared_ptr<vsomeip::application> app_;
     std::thread timer_thread_;
     std::atomic<bool> running_;
+    
+    void send_speed_event(){
+        
+        float speed = 65.5f;
+        std::vector<vsomeip::byte_t> payload_data(4);
+        memcpy(payload_data.data(), &speed, sizeof(speed));
+        auto payload = runtime_->create_payload(payload_data);
+        app_->notify(0x1234, 0x0001, 0x8001, payload);
+    }
     public:
     SpeedProvider(){
         runtime_ = vsomeip::runtime::get();
@@ -25,13 +35,25 @@ class SpeedProvider{
         if(_state==vsomeip::state_type_e::ST_REGISTERED){
             std::cout<<"Registered with routing manager"<<std::endl;
             app_->offer_service(0x1234, 0x0001);
+            std::set<vsomeip::eventgroup_t> eventgroup = {0x0001};
+            app_->offer_event(0x1234, 0x0001, 0x8001, eventgroup, vsomeip::event_type_e::ET_FIELD);
+            running_ = true;
+
+            timer_thread_ = std::thread([this](){            
+                while(running_){
+                send_speed_event();
+                std::this_thread::sleep_for(std::chrono::milliseconds(200));
+                };
+                }
+            );
         }
         };
         app_->register_state_handler(res);
-        return false;
+        return true;
     }
 
     void start(){
+
         app_->start();
     }
 
