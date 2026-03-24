ff02c26b5a78a0007b5e25b9cea61ccab9ed75fd
stawank <stawank@gmail.com>
2026-03-24 14:27:50 +0100

    feat: offer_service 0x1234 on ST_REGISTERED

--- src/speed_service/speed_provider.cpp
+++ src/speed_service/speed_provider.cpp
@@ -1,9 +1,52 @@
 #include <iostream>
 #include <vsomeip/vsomeip.hpp>
+#include <vsomeip/runtime.hpp>
+#include <thread>
+#include <atomic>
+
+class SpeedProvider{
+    
+    std::shared_ptr<vsomeip::runtime> runtime_;
+    std::shared_ptr<vsomeip::application> app_;
+    std::thread timer_thread_;
+    std::atomic<bool> running_;
+    public:
+    SpeedProvider(){
+        runtime_ = vsomeip::runtime::get();
+        app_ = runtime_->create_application("SpeedProvider"); 
+        running_ = false;
+    }
+
+    bool init(){
+        if(!app_->init()){
+            return false;
+        };
+        auto res = [this](vsomeip::state_type_e _state){
+        if(_state==vsomeip::state_type_e::ST_REGISTERED){
+            std::cout<<"Registered with routing manager"<<std::endl;
+            app_->offer_service(0x1234, 0x0001);
+        }
+        };
+        app_->register_state_handler(res);
+        return false;
+    }
+
+    void start(){
+        app_->start();
+    }
+
+
+
+
+
+
+};
 
 int main(){
 
-    std::cout<<"First run of speed provider"<< std::endl;
+    SpeedProvider speed_provider;
+    speed_provider.init();
+    speed_provider.start();
 
     return 0;
 }
